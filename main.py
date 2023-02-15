aea8afafd2942baa47fa6eb9f6375146941578da
Osvel Alvarez <oalvarez@sisinfo.com.uy>
2023-02-15 18:17:52 -0300

    [ADD] Some style to HTML message

--- main.py
+++ main.py
@@ -30,11 +30,16 @@ def get_section_path(section, include_section=False):
     return path + section
 
 
-def setup_files():
+def get_files_folder():
     files_folder = config['DEFAULT']['files_folder']
     if not files_folder.endswith("/"):
         files_folder += "/"
 
+    return files_folder
+
+
+def setup_files():
+    files_folder = get_files_folder()
     for section in sections:
         path_from = files_folder + section
         path_to = get_section_path(section)
@@ -50,42 +55,88 @@ def check_files():
     This function checks every file and return true if some file has been accessed
     """
     for section in sections:
+        # Get the original files lists
+        section_folder_path = get_files_folder() + section
+        section_files_path = subprocess.Popen(
+            ['find', section_folder_path], stdout=subprocess.PIPE
+        ).communicate()[0].decode("utf-8").replace(section_folder_path, "").splitlines()
+
         path = get_section_path(section, True)
         output = subprocess.Popen(['find', path], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").splitlines()
+
+        accessed_files = []
         for file in output:
+            # Ignore any file that isnt in original files path
+            if file.replace(path, "") not in section_files_path:
+                continue
+            # Ignore directories
             if os.path.isdir(file):
                 continue
-            if check_file_accessed(file):
-                return True
+            # Check if file has been accessed
+            accessed_time = check_file_accessed(file)
+            if accessed_time:
+                accessed_files.append((file, accessed_time))
+
+        return accessed_files
 
 
 def check_file_accessed(path):
     output = subprocess.Popen(['stat', path], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").splitlines()
     access = re.sub(".*: ", "", output[4])
     birth = re.sub(".*: ", "", output[7])
-    return access != birth
+    return access if access != birth else ""
 
 
-def send_alert():
-    # subprocess.Popen(['stat', path], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").splitlines()
+def send_alert(accessed_files=None):
     print("some files have been accessed")
-    send_email()
+    send_email(accessed_files)
 
 
-def send_email(test_message=False):
+def send_email(accessed_files=None, test_message=False):
     sender = config['DEFAULT']['gmail_sender']
     receiver = config['DEFAULT']['receiver_email']
-    subject = """Intruder alert"""
-    message = """
-        You have been hacked!!!!<br><br>    
-        Some of your honeypot files have been accessed recently.
-    """
 
     if test_message:
         subject = """Testing Python message"""
         message = """This message is sent from Python."""
+    else:
+        subject = """Intruder alert"""
+        message = get_body_message(accessed_files)
 
     gmail_helper.send_message(sender, receiver, subject, message)
+
+
+def get_body_message(accessed_files=None):
+    if accessed_files is None:
+        return """
+            Ops!!! Something went wrong, but keep alert ;)
+        """
+
+    body = """
+        <h2>You have been hacked!!!</h2>
+        <br>The next honeypots files has been accessed:<br><br>
+        <table style="font-family: arial, sans-serif; border-collapse: collapse;">
+          <tr>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;" width="40%">Timestamp</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;" width="60%">File Path</th>
+          </tr>
+    """
+
+    styled_row = True
+    for (path, accessed) in accessed_files:
+        if styled_row:
+            body += """<tr style="background-color: #dddddd;">"""
+        else:
+            body += "<tr>"
+
+        body += f"""
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{accessed}</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">{path}</td></tr>
+        """
+
+    body += "</table>"
+
+    return body
 
 
 def read_config():
@@ -104,8 +155,9 @@ def read_config():
 
 
 def run():
-    if check_files():
-        send_alert()
+    accessed_files = check_files()
+    if len(accessed_files) != 0:
+        send_alert(accessed_files)
 
     # Setup files again after check old files
     if sys.argv.__contains__("--setup"):
